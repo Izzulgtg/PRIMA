7497f05c49f00fd97f71a624f253de78109aff57
Izzul <izzulmillahaqil@gmail.com>
2026-06-06 21:31:49 +0700

    update sidebar admin

--- Frontend/src/components/navigation/sidebar.jsx
+++ Frontend/src/components/navigation/sidebar.jsx
@@ -1,9 +1,25 @@
 import { sidebarMenus } from "../../data/sidebar-menu"
 import SidebarItem from "./sidebar-item"
+import { LogOut } from "lucide-react"
+import { useNavigate } from "react-router-dom"
 
 function Sidebar() {
-  const role = "admin"
-  const menus = sidebarMenus[role]
+ const user = JSON.parse(
+  localStorage.getItem("user")
+)
+
+const role = user?.role
+
+const menus = sidebarMenus[role] || []
+
+const navigate = useNavigate()
+
+const handleLogout = () => {
+
+  localStorage.clear()
+
+  navigate("/login")
+}
   return (
     <aside className="w-64 min-h-screen bg-white border-r border-[#EDE8DC]">
 
@@ -13,16 +29,40 @@ function Sidebar() {
         </h1>
       </div>
 
-      <div className="px-4 space-y-2">
-        {menus.map((menu) => (
-         <SidebarItem
-            key={menu.path}
-            title={menu.title}
-            icon={menu.icon}
-            path={menu.path}
-            />
-        ))}
-      </div>
+<div className="flex flex-col justify-between h-[85vh]">
+
+  <div className="px-4 space-y-2">
+    {menus.map((menu) => (
+      <SidebarItem
+        key={menu.path}
+        title={menu.title}
+        icon={menu.icon}
+        path={menu.path}
+      />
+    ))}
+  </div>
+
+  <div className="px-4 pb-6">
+
+    <button
+      onClick={handleLogout}
+      className="
+        flex items-center gap-3
+        w-full
+        px-4 py-3
+        rounded-xl
+        hover:bg-red-50
+        text-red-500
+        transition
+      "
+    >
+      <LogOut size={20} />
+      <span>Logout</span>
+    </button>
+
+  </div>
+
+</div>
 
     </aside>
   )
